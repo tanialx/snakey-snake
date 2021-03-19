91c7815b687137916888c9db029b770e8050546c
tanialx <taniathucle@gmail.com>
2021-03-19 17:50:06 +0700

    Minor color changes

--- snake.js
+++ snake.js
@@ -2,11 +2,11 @@ class Snake {
 
     constructor(snakeboard_ctx, initialPosition) {
         this.snake_body = initialPosition;
-        this.snake_col = 'DarkSeaGreen';
-        this.snake_border = 'Black';
-        this.snake_tile_w = 19;
-        this.snake_tile_h = 19;
-        this.snake_tile_corner_radius = 8;
+        this.snake_col = '#d1e0e0';
+        this.snake_border = 'White';
+        this.snake_tile_w = 20;
+        this.snake_tile_h = 20;
+        this.snake_tile_corner_radius = 4;
         this.ctx = snakeboard_ctx;
         this.step = 20;
         this.dx = this.step;
@@ -27,8 +27,8 @@ class Snake {
             snake_part.x, snake_part.y,
             this.snake_tile_w, this.snake_tile_h,
             this.snake_tile_corner_radius,
-            this.snake_col, 0,
-            1);
+            this.snake_col, this.snake_border,
+            2);
 
     }
 
