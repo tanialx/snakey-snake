cafca42da47b1f5c4aa16de1d3d308c611b07f77
tanialx <taniathucle@gmail.com>
2021-03-26 12:03:46 +0700

    Fruit: minor refactoring

--- fruit.js
+++ fruit.js
@@ -44,18 +44,22 @@ class Fruit {
             x: this.x,
             y: this.y - this.fruitSize / 4
         }
+        const stalk_mid = {
+            x: stalk_root.x + 2 * offset,
+            y: stalk_root.y - 3 * offset
+        }
         this.ctx.beginPath();
         this.ctx.lineWidth = 1;
         this.ctx.moveTo(stalk_root.x, stalk_root.y);
-        this.ctx.quadraticCurveTo(stalk_root.x, stalk_root.y - offset, stalk_root.x + 2 * offset, stalk_root.y - 3 * offset);
+        this.ctx.quadraticCurveTo(stalk_root.x, stalk_root.y - offset, stalk_mid.x, stalk_mid.y);
         this.ctx.quadraticCurveTo(stalk_root.x + 4 * offset, stalk_root.y - 6 * offset, stalk_root.x + 6 * offset, stalk_root.y - 6 * offset);
         this.ctx.stroke();
 
         // Leaf
         this.ctx.beginPath();
-        this.ctx.moveTo(stalk_root.x + 2 * offset, stalk_root.y - 3 * offset);
+        this.ctx.moveTo(stalk_mid.x, stalk_mid.y);
         this.ctx.quadraticCurveTo(stalk_root.x, stalk_root.y - 7 * offset, stalk_root.x - 4 * offset, stalk_root.y - 5 * offset);
-        this.ctx.quadraticCurveTo(stalk_root.x - 2 * offset,  stalk_root.y - 3 * offset, stalk_root.x + 2 * offset,  stalk_root.y - 3 * offset);
+        this.ctx.quadraticCurveTo(stalk_root.x - 2 * offset, stalk_root.y - 3 * offset, stalk_mid.x, stalk_mid.y);
         this.ctx.lineTo(stalk_root.x - 2 * offset, stalk_root.y - 4 * offset);
         this.ctx.fillStyle = '#66CDAA';
         this.ctx.fill();
